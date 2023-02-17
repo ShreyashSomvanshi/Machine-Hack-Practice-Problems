5d8cfa4d7f569b583a5972278bc980918343a622
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-02-17 18:19:57 +0530

    Create 21. Length of a dictionary.py

---
+++ Dictionaries/21. Length of a dictionary.py
@@ -0,0 +1,18 @@
+'''
+Consider a dictionary as dict1 consisting of n-number of keys and corresponding values. Find the length of the dictionary.
+
+Example 1: 
+Input:  dict1 = {"a","b","c"}
+Output: 3
+ 
+Example 2: 
+Input: dict1 = {"a":1,"b":2,"c":3,"d":4}
+Output: 4
+'''
+
+# Solution:
+def lengthOfDictionary(dict1):
+  return len(dict1)
+  
+print(lengthOfDictionary(dict1))
+
