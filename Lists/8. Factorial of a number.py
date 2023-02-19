c1613dc716c3247a7b788071331d78ffb651419f
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-02-19 13:30:45 +0530

    Create 8. Factorial of a number.py

---
+++ Lists/8. Factorial of a number.py
@@ -0,0 +1,22 @@
+'''
+Obtain the factorial of the given number N.
+
+Example 1: 
+Input:  N = 2
+Output: 2
+ 
+Example 2: 
+Input:N = 5
+Output: 120
+'''
+
+# Solution: 
+
+
+
+# Simplest solution:
+def factorial(N):
+  import math
+  return math.factorial(N)
+  
+print(factorial(N))
