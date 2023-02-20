137283dd98904194d60687608041146edfd48efb
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-02-20 19:11:22 +0530

    Update 14. Frequency of characters in a string.py

--- Dictionaries/14. Frequency of characters in a string.py
+++ Dictionaries/14. Frequency of characters in a string.py
@@ -4,7 +4,19 @@
 '''
 
 # Solution:
-
+def character_frequency(string1):
+  all_freq = {}
+ 
+  for i in string1:
+    if i in all_freq:
+      all_freq[i] += 1
+    else:
+      all_freq[i] = 1
+      
+  return all_freq
+        
+        
+print(character_frequency(string1))
 
 # Referred from:
 # https://www.geeksforgeeks.org/python-frequency-of-each-character-in-string/
