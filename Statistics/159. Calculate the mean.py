686e6e03ac11449664b0156d9bb8d4c6b4065b96
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-02-22 22:25:30 +0530

    Create 159. Calculate the mean.py

---
+++ Statistics/159. Calculate the mean.py
@@ -0,0 +1,12 @@
+'''
+Consider the data as “speed” made up of different speeds of a car. Now write a program to print the average speed of the car. 
+
+Example 1: 
+Input:  speed = [90,91,92,99,83,89]
+Output: 90.66666666666667
+ 
+Example 2: 
+Input: speed = [126,100,129,139]
+Output: 123.5
+'''
+# Solution:
