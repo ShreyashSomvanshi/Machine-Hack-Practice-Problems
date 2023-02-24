f6d18e900291722da96c7b3e2efae6424e9a2c7b
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2023-02-24 18:03:29 +0530

    Create 160. Calculate the Median.py

---
+++ Statistics/160. Calculate the Median.py
@@ -0,0 +1,73 @@
+'''
+Find the median of the given data data1 containing weights of different persons.
+
+Example 1:
+Input: data1 = [45,46,47,48]
+Output: 46.5
+
+Example 2:
+Input: data1 = [95,46,90,99,53]
+Output: 90
+'''
+
+# My Solution:
+import statistics
+
+def median(data1):  
+# First sort in ascending order; Median = {(n + 1) / 2}th value for n=even,
+# Median = n/2 for n=odd but adjust by -1 as indices start with 0
+  j = sorted(data1)
+  length = len(j)
+  if (length % 2) == 0:
+    med = int((length/2))
+    med2 = int((length/2)-1)
+    d = j[med]
+    o = j[med2]
+    g = []
+    g.append(d)
+    g.append(o)
+    avg = sum(g)/2
+    
+  else:
+    g = int((length+1)/2)
+    avg = j[g-1]
+    
+  return avg
+
+print(median(data1))
+
+
+
+# My Approach:
+
+'''
+def median(data1):  # First sort in ascending order; Median = {(n + 1) / 2}th value for n=even, Median = n/2 for n=odd
+  j = sorted(data1)
+  length = len(j)
+  if (length % 2) == 0:
+    med = int((length/2))
+    med2 = int((length/2)-1)
+    d = j[med]
+    print(d)
+    o = j[med2]
+    g = []
+    g.append(d)
+    g.append(o)
+    print(g)
+    avg = sum(g)/2
+    
+    
+  else:
+    avg = (length+1)/2
+    
+#   med = int(med)
+  return avg
+  
+data1 = [45,46,47,48]
+# Output:
+
+# 46.5
+# j = sorted(data1)
+print(median(data1))
+
+'''
